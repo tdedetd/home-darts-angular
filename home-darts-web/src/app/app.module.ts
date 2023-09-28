3b8b8ec3c1589a421233876f9726fce827144ee7
tdedetd <tdedetd@gmail.com>
2023-09-29 00:50:27 +0300

    front. atc start screen material

--- home-darts-web/src/app/app.module.ts
+++ home-darts-web/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MenuComponent } from './components/menu/menu.component';
@@ -22,6 +23,7 @@ import { CdkMenuModule } from '@angular/cdk/menu';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     StoreModule.forRoot(),
     EffectsModule.forRoot([GameInfoEffects]),
     StoreDevtoolsModule.instrument({ logOnly: true }),
